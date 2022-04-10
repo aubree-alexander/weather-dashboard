07a9c4c2b96775b30fe3f5ee51d5bb55b1234c3e
Aubree Alexander <aubree.alexander@gmail.com>
2022-04-09 22:44:10 -0500

    append forecast data to container, add card styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,28 +36,44 @@ function getCoord(city){
                 })
         })
 }
-
 getCoord("edison");
+
+//backtrack to here 
 
 //get current weather data for city entered 
 function getCurrent(city, data) {
     var time = data.current.dt
-    var date = dajs().tz(data.timezone).format('M/D/YYYY')
+    var date = dayjs().tz(data.timezone).format('M/D/YYYY')
     var displayWeather = $("#currentDayWeather")
-    var weatherColumn = $("#weatherColumn")
-    var weatherCard = $("#weatherCard")
-    var cityTitle = $("#cityTitle")
-    var temp = $("#temp")
-    var humidity = $("#temp")
-    var wind = $("$wind")
-    var uv = $("$uvIndex")
+    var weatherColumn = document.createElement('div')
+    var weatherCard = document.createElement('div')
+    var cityTitle = document.createElement('h2')
+    var temp = document.createElement('p')
+    var humidity = document.createElement('p')
+    var wind = document.createElement('p')
+    var uv = document.createElement('p')
+    weatherColumn.setAttribute('class', 'col-md')
+    weatherCard.setAttribute('class', 'card-body float-left')
 
     cityTitle.innerText = city + " " + date
-    temp.innerText = data.current.temp + " °F"
+    temp.innerText = "Temp: " + data.current.temp + " °F"
+    humidity.innerText = "Humidity: " + data.current.humidity + "%"
+    wind.innerText = "Wind: " + data.current.wind_speed + " MPH"
+    uv.innerText = "UV Index: " + data.current.uvi
     displayWeather.append(weatherColumn)
     weatherColumn.append(weatherCard)
-    weatherCard.append(cityTitle, temp)
+    weatherCard.append(cityTitle, temp, humidity, wind, uv)
+    
+    //display different colors depending on value of uv index
+    if (data.current.uvi <= 2) {
+        uv.setAttribute('class', 'bg-success')
+    } else if (data.current.uvi <=5) {
+        uv.setAttribute('class', 'bg-warning')
+    } else {
+        uv.setAttribute('class', 'bg-danger')
+    }
 }
+
 
 
 //5-day forecast cards, starting at tomorrow's date
@@ -69,29 +85,41 @@ function fiveDay(data) {
 
     for (i = 1; i < 6; i++) {
         var cardDate = document.createElement('div')
-        date.textContent = dayjs.unix(data.daily[i].dt).tz(data.timezone).format('M/D/YYYY')
+        cardDate.textContent = dayjs.unix(data.daily[i].dt).tz(data.timezone).format('M/D/YYYY')
         var cardTemp = document.createElement('p')
         var cardHumidity = document.createElement('p')
         var cardWind = document.createElement('p')
-        fiveDayCol.append(date)
+          
+        fiveDayCol.append(cardDate)
+        //adding stuff here
+        var dataTemp = data.daily[0].temp.day
+        var dataHumidity = data.daily[0].humidity
+        var dataWind = data.daily[0].wind_speed
+        cardTemp.innerText = "Temp: " + dataTemp + " °F"
+        cardHumidity.innerText = "Humidity: " + dataHumidity + "%"
+        cardWind.innerText = "Wind: " + dataWind + " MPH"
+        
+        cardHolder.append(cardTemp, cardHumidity, cardWind)
+        
+
     }
 }
 
 
-//click event for search button, save to localstorage
-$("#searchBtn").click(function() {
-    var cityName = $.trim($("#searchBar").val())
-    var searchItemContainer = document.createElement("div")
-    searchItemContainer.classList = "list-item flex-row justify-space-between align-center"
-    var searchItemHistory = document.createElement("span")
-    searchItemHistory.classList = "flex-row align-center"
-    // searchEl.textContent = cityName.innerText
-    searchItemContainer.appendChild(searchItemHistory)
-    searchHistory.appendChild(searchItemContainer)
 
-    localStorage.setItem('cityName', cityName)
+// //click event for search button, save to localstorage
+// $("#searchBtn").click(function() {
+//     var cityName = $.trim($("#searchBar").val())
+//     getCoord(cityName)
+//     // var searchItemContainer = document.createElement("div")
+//     // searchItemContainer.classList = "list-item flex-row justify-space-between align-center"
+//     var searchItemHistory = document.createElement("span")
+//     searchItemHistory.classList = "flex-row align-center"
+//     searchItemContainer.append(searchItemHistory)
+//     searchHistory.append(searchItemContainer)
 
-})
+//     localStorage.setItem('cityName', cityName)
+// })
 
 
 //https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
