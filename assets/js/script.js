eb0fb8a809c11038bba6d9844090bc9b431c221e
Aubree Alexander <aubree.alexander@gmail.com>
2022-04-09 20:00:52 -0500

    add 5-day forecast function, current city/current day function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,69 +1,97 @@
+//dayjs extensions
+dayjs.extend(window.dayjs_plugin_utc);
+dayjs.extend(window.dayjs_plugin_timezone);
+
+//global variables
 var cityName = '';
 var apiKey = "d91f911bcf2c0f925fb6535547a5ddc9";
 //"a6afda3f25e97c43efdc2ed9326eeba8";
+var searchHistoryItem = $("#searchHistoryItem")
+var searchHistory = $("searchHistory")
 
 
-// "http://api.openweathermap.org/geo/1.0/direct?q=edison&limit=5&appid=a6afda3f25e97c43efdc2ed9326eeba8"
 
-
-
+//function to fetch coordinates from weather api
 function getCoord(city){
     var apiCoordURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=5&appid=" + apiKey ;
 
     fetch(apiCoordURL)
-        .then(function(res){
-            return res.json();
+        .then(function(response){
+            return response.json();
         })
+        //convert latitude and longitude to city name
         .then(function(data){
-            var latitude = $.trim(data[0].lat)
-            var longitude = $.trim(data[0].lon)
-            localStorage.setItem('latitude', latitude)
-            localStorage.setItem('longitude', longitude)
-            // console.log(data[0].lat)
-            // console.log(data[0].lon)
-
-            //document.getElementById("test").innerHTML = data[0].state;
-            $("#test").html(data[0].state);
-        })
-        .catch(function(err){
-            console.log(err);
+            var apiForecastURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data[0].lat + "&lon=" + data[0].lon + "&units=imperial&exclude=minutely,hourly,alerts&appid=" + apiKey ;
+            fetch(apiForecastURL)
+                .then(function(response){
+                    return response.json();
+                })
+                .then(function(data){
+                    console.log(data)
+                    fiveDay(data);
+                    getCurrent(city, data);
+                })
+                .catch(function(err){
+                    console.log(err);
+                })
         })
 }
 
 getCoord("edison");
 
+//get current weather data for city entered 
+function getCurrent(city, data) {
+    var time = data.current.dt
+    var date = dajs().tz(data.timezone).format('M/D/YYYY')
+    var displayWeather = $("#currentDayWeather")
+    var weatherColumn = $("#weatherColumn")
+    var weatherCard = $("#weatherCard")
+    var cityTitle = $("#cityTitle")
+    var temp = $("#temp")
+    var humidity = $("#temp")
+    var wind = $("$wind")
+    var uv = $("$uvIndex")
+
+    cityTitle.innerText = city + " " + date
+    temp.innerText = data.current.temp + " °F"
+    displayWeather.append(weatherColumn)
+    weatherColumn.append(weatherCard)
+    weatherCard.append(cityTitle, temp)
+}
 
 
+//5-day forecast cards, starting at tomorrow's date
+function fiveDay(data) {
+    var fiveDayDiv = $("#fiveDay")
+    var fiveDayCol = document.createElement('div')
+    fiveDayCol.setAttribute('class', 'col-12')
+    fiveDayDiv.append(fiveDayCol)
+
+    for (i = 1; i < 6; i++) {
+        var cardDate = document.createElement('div')
+        date.textContent = dayjs.unix(data.daily[i].dt).tz(data.timezone).format('M/D/YYYY')
+        var cardTemp = document.createElement('p')
+        var cardHumidity = document.createElement('p')
+        var cardWind = document.createElement('p')
+        fiveDayCol.append(date)
+    }
+}
 
 
 //click event for search button, save to localstorage
 $("#searchBtn").click(function() {
     var cityName = $.trim($("#searchBar").val())
+    var searchItemContainer = document.createElement("div")
+    searchItemContainer.classList = "list-item flex-row justify-space-between align-center"
+    var searchItemHistory = document.createElement("span")
+    searchItemHistory.classList = "flex-row align-center"
+    // searchEl.textContent = cityName.innerText
+    searchItemContainer.appendChild(searchItemHistory)
+    searchHistory.appendChild(searchItemContainer)
+
     localStorage.setItem('cityName', cityName)
+
 })
 
 
 //https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-
-
-
-// Step 3. create fetch function to the weather API to return data about current and future weather conditions
-    // Step 3.5. Include data about city name, date, weather condition icon (fontAwesome?) temp, humidity, wind speed, UV index
-// Step 4. create function to save search data to localStorage
-// Step 5. create function to append search history to page
-    // Step 5.5 use JQuery to turn search history into clickable buttons that return data
-// Step 6. create if statement to determine UV index value and color code accordingly
- 
-
-
-
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
